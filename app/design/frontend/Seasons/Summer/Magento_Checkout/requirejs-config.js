f3cb623f5f652ed5275c2178c4deeb3ae059ce59
Andrii Batoh <batoh.dev@gmail.com>
2020-01-05 00:46:50 +0200

    Added floated labels for checkout steps input fields

---
+++ app/design/frontend/Seasons/Summer/Magento_Checkout/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+  config: {
+    mixins: {
+      'Magento_Checkout/js/view/billing-address': {
+        'Magento_Checkout/js/mixin/view/billing-address': true
+      },
+      'Magento_Checkout/js/view/shipping': {
+        'Magento_Checkout/js/mixin/view/shipping-mixin': true
+      }
+    }
+  }
+};
