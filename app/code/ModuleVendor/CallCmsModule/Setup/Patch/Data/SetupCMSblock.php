2a595a5458b0f29b26a6bcd647cb19fe65691e12
Andrii Batoh <batoh.dev@gmail.com>
2020-01-17 17:55:00 +0200

    Create CMS block with patch

---
+++ app/code/ModuleVendor/CallCmsModule/Setup/Patch/Data/SetupCMSblock.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ModuleVendor\CallCmsModule\Setup\Patch\Data;
+
+use \Magento\Cms\Model\BlockFactory;
+use \Magento\Framework\Setup\Patch\DataPatchInterface;
+use \Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class SetupCMSblock implements DataPatchInterface
+
+{
+    protected $blockFactory;
+
+    protected $moduleDataSetup;
+
+    public function __construct(
+        BlockFactory $blockFactory,
+        ModuleDataSetupInterface $moduleDataSetup
+    ) {
+        $this->blockFactory = $blockFactory;
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    public function apply() {
+
+        $html = '<a href="#">Cms block</a>';
+        $data = [
+            'title' => 'Some CMS block',
+            'identifier' => 'some_cms',
+            'content' => $html,
+            'is_active' => 1,
+            'stores' => [0],
+            'sort_order' => 0
+        ];
+
+        $applyData = [$data];
+
+        $this->moduleDataSetup->getConnection()->startSetup();
+        try {
+            $this->createBlock($applyData);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function createBlock($dataArray) {
+        foreach ($dataArray as $blockData) {
+            $block = $this->blockFactory->create()->load($blockData['identifier']);
+            if (!$block->getId()) {
+                $block->setData($blockData)->save();
+            } else {
+                $block->setData('content', $blockData['content'])->save();
+            }
+        }
+    }
+
+    public function revert() {}
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
