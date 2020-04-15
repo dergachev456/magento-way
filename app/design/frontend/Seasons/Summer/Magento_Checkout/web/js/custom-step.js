fc9af0ae4ac71bdd673bc2e59a4d5b162c374687
Andrii Batoh <batoh.dev@gmail.com>
2020-04-15 18:53:48 +0300

    Display custom step before other steps

--- app/design/frontend/Seasons/Summer/Magento_Checkout/web/js/custom-step.js
+++ app/design/frontend/Seasons/Summer/Magento_Checkout/web/js/custom-step.js
@@ -51,7 +51,7 @@ define(
                      * 10 < 'sort order value' < 20 : step displays between shipping and payment step
                      * 'sort order value' > 20 : step displays after payment step
                      */
-                    15
+                    9
                 );
 
                 return this;
